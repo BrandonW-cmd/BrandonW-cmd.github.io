5a7e07e78972d24f26580d5c11cc9ee087cad8fc
BrandonW-cmd <809bsw@gmail.com>
2024-08-07 16:34:44 +0000

    added divs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let myButton = document.querySelector("button")
 let dark = document.querySelector("#darkMode")
 
 function DarkMd(){
-    document.body.style.backgroundColor = `rgb(0, 0, 0)`
+    document.body.style.backgroundColor = `rgb(7,7,7)`
 }
 
 dark.addEventListener("click", DarkMd)
